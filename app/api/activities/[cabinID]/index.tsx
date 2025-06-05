3249782d3795458d834488151ce83dba681b6207
Matt-Hamilton-GitHub <i.w.b.s.bilm@gmail.com>
2025-06-04 20:23:49 -0700

    new api rotes for fetching groups and activities added

---
+++ app/api/activities/[cabinID]/index.tsx
@@ -0,0 +1,3 @@
+export async function GET(req: Request, params : {params : {cabinID: string}}){
+// return all activities availabel for a cabin
+}
---
+++ app/api/groups/[userID]/route.tsx
@@ -0,0 +1,4 @@
+
+async function GET(req: Request, params: {userID : string}){
+
+}
